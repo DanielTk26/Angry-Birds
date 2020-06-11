a786b3c730e90288655faf32d90df9185ad75523
Daniel Thomas <63409207+DanielTk26@users.noreply.github.com>
2020-06-11 16:34:36 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,13 +7,14 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+
 var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
-function setup() {
+function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
@@ -69,18 +70,19 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
     gameState = "launched";
-
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
 }
